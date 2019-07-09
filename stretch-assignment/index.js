6c711ed06ca72d22005bb4870f9ef1a90b762202
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2019-07-09 15:57:39 -0500

    added mouseup listener to stretch

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,9 +1,3 @@
-// const red = document.querySelector(".block--red");
-// const blue = document.querySelector(".block--blue");
-// const green = document.querySelector(".block--green");
-// const pink = document.querySelector(".block--pink");
-// const gray = document.querySelector(".block--gray");
-
 const blocks = document.querySelectorAll(".block");
 blocks.forEach(function(block) {
   block.addEventListener("click", event => {
@@ -12,15 +6,16 @@ blocks.forEach(function(block) {
     });
     event.target.style.order = "0";
   });
-});
-
-blocks.forEach(function(block) {
+  let pos = 0;
   block.addEventListener("mousedown", event => {
-    let pos = 0;
     let id = setInterval(frame, 10);
     function frame() {
       pos++;
       event.target.style.left = pos + "px";
     }
+
+    block.addEventListener("mouseup", event => {
+      clearInterval(id);
+    });
   });
 });
